d8fa2c2a5b54014174524c95a6cdc9e78682b773
Sébastien Bonami <sbonami@ferlab.bio>
2024-06-07 15:57:18 -0400

    feat: INFRA-403 improve script to show log of instances in openstack

--- openstack/list_instances_lastlogline.sh
+++ openstack/list_instances_lastlogline.sh
@@ -6,11 +6,23 @@ else
     instance_names=$(openstack server list --sort name:asc -c Name -f value)
 fi
 
-echo -e "INSTANCE NAME\t\t\tLAST LOG LINE"
+echo -e "INSTANCE NAME\t\t\t\tLAST LOG LINE"
 
+down_instances=()
 for name in $instance_names
 do
-    command=$(timeout 5 openstack console log show "$name" | tail -n 1)
-    echo -e "$name\t\t$command" | tr '\n' '\t'
-    echo ""
+    log=$(timeout 5 openstack console log show "$name")
+    if [ $? -eq 124 ]; then
+        down_instances+=("$name")
+    else
+        last_line_log=$(echo "$log" | tail -n 1)
+        echo -e "$name\t\t\t\t$last_line_log" | tr '\n' '\t'
+        echo ""
+    fi
+done
+
+echo -e "\nInstances that appear to be down (can't retrieve their logs):"
+for instance in "${down_instances[@]}"
+do
+    echo "$instance"
 done
